ca32adef28ba2ccb2e840788dbe656aff60791d4
Reyhan Çolak <reyhancolak1324@gmail.com>
2023-05-10 00:26:04 +0300

    sehir-tanitimi-html
    web teknolojileri proje

--- web proje/proje.php
+++ web proje/proje.php
@@ -1,1 +1,35 @@
-proje.php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body> 
+  
+<?php  
+  $ema = 'g221210036@sakarya.edu.tr';
+  $pass = '221210036';
+   
+     if(isset($_POST['email'])){
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        if($email==$ema && $password==$pass){
+            echo "GİRİŞ BAŞARILI";
+        }
+        else {
+            echo "HATALI GİRİŞ";
+        }
+
+     }
+     
+   
+  
+
+    
+    
+    ?>
+</body>
+</html>
