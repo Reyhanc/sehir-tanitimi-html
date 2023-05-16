fa7911733002507a2f467b5bafaad446d468e32f
Reyhan Çolak <reyhancolak1324@gmail.com>
2023-05-16 21:51:53 +0300

    sehir-tanitimi-html
    web proje

--- web proje/proje.php
+++ web proje/proje.php
@@ -25,11 +25,43 @@
 
      }
      
-   
-  
-
-    
-    
     ?>
+
+
+<script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
+<script>
+  const app = new Vue({
+    el: '#contact-form',
+    methods: {
+      checkName() {
+        // İsim kontrolü işlemlerini gerçekleştirin
+      },
+      checkEmail() {
+    echo "isim eşleşti";
+      }
+    }
+  });
+
+  methods: {
+  checkName() {
+    const name = document.getElementById('name-input').value;
+    if (name.trim() === 'Reyhan') {
+      alert('Lütfen adınızı girin!');
+    } else {
+      // İsim geçerli, istediğiniz işlemleri yapabilirsiniz
+    }
+  }
+  checkEmail() {
+    const email = document.getElementById('email-input').value;
+    const emailRegex = /^\S+@\S+\.\S+$/;
+    if (!emailRegex.test(email)) {
+      alert('Lütfen geçerli bir e-posta adresi girin!');
+    } else {
+        echo "e-posta eşleşti";
+    }
+  }
+}
+</script>
+
 </body>
 </html>
